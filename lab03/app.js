fd4644b1bb49e92b818813579b0d3bd9e2024f07
SeanJacobsma <43056022+SeanJacobsma@users.noreply.github.com>
2018-09-22 01:55:35 -0400

    Add files via upload

--- lab03/app.js
+++ lab03/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -12,3 +13,4 @@ app.get('/', (req, res) => res.redirect('/index.html'))
 
 app.use(express.static('public'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
