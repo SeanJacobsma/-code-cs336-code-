81b4d4d047709f85a7e9aee507cd310231919410
SeanJacobsma <43056022+SeanJacobsma@users.noreply.github.com>
2018-09-22 01:54:52 -0400

    Create app.js

---
+++ lab03/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.redirect('/index.html'))
+//app.get('/', (req, res) => res.send('Express.js is a framework for building web applications based on node.js.'))
+//app.get('/', (req, res) => res.send('A static file does not need to be modified or processed.'))
+//app.get('/', (req, res) => res.send('The server does not continue to deliver up Hello World, it is loaded once, and every time the server is reloaded'))
+
+
+
+
+app.use(express.static('public'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
